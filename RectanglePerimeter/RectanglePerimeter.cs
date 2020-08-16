711cf106b36110a2a44c018f4e603274160a052d
anilthirumala <anilthirumala@gmail.com>
2020-08-16 12:44:34 +0530

    Initial Commit

---
+++ RectanglePerimeter/RectanglePerimeter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RectanglePerimeter
+{
+    class RectanglePerimeter
+    {
+        static void Main(string[] args)
+        {
+            new RectanglePerimeter().GetValues();
+        }
+        //get user input from console
+        public void GetValues()
+        {
+
+        }
+
+        //write logic to find perimeter of rectangle here
+        public double PerimeterCalculator(double length, double breadth)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Test/RectanglePerimeterTest.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Test
+{
+    public class RectanglePerimeterTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
